af5765cc383d988521dcfa38792af2949f0e987c
caique-js <60150005+caique-js@users.noreply.github.com>
2020-04-14 13:51:38 -0300

    Add files via upload

---
+++ NinjaPixel/features/step_definitions/login_steps.rb
@@ -0,0 +1,27 @@
+Dado("que acesso a pagina principal") do
+    visit "http://pixel-web:3000/login"
+    page.current_window.resize_to(1366, 768)
+end
+  
+Quando("eu submeto minhas credenciais {string} e {string}") do |email, password|
+    # O hastag é para buscar um elemento pelo ID (Inspecione o elemento para ver)
+    find("#emailId").set email
+    find("input[name=password]").set password
+    click_button "Entrar"
+end
+  
+Então("devo ser autenticado") do
+    logged_user = find(".user .info span").text
+    expect(logged_user).to eql "Papito"
+end
+
+Quando("eu submeto minhas credenciais com senha inválida") do
+    find("#emailId").set "papito@ninjapixel.com"
+    find("input[name=password]").set "´pwd123456789"
+    click_button "Entrar"
+end
+  
+Então("devo ver uma mensagem de alerta {string}") do |string|
+    card_error = find(".alert-dismissible span").text
+    expect(card_error).to eql "Usuário e/ou senha inválidos"
+end
